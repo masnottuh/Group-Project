58eff2d2832f340423e87bfa88462e5ba7be4f5d
lizcoats <lcclujana@gmail.com>
2023-03-16 22:03:37 -0400

    added Carousel/needs work

--- frontend/src/NavPages/Home.js
+++ frontend/src/NavPages/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useState, useEffect } from 'react'
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import Carousel from'react-bootstrap/Carousel'
 import axios from "axios"
+import { MDBCard} from 'mdb-react-ui-kit';
 const Home = () =>{
   const[books, setBooks] = useState([])
   
@@ -14,49 +18,88 @@ const Home = () =>{
   getBooks()
 }, [])
     
-return (
-    <div className="centered">
+// return (
+//     <div className="centered">
+//      <h1>Books</h1>
+//       <div>
      
-      <div>
-        <h1>Books</h1>
-        <section>
+//          {books.map((book) =>{
+//             const{author, book_image, buy_links, primary_isbn10,description, publisher, rank, title}= book
+            
+//             return(
+//               <div>
+//               <Card key ={rank} bg='secondary'style={{width: '18rem'}}>
+//               <Card.Body>
+//                 <div>
+//                     <Card.Img className ="image_book" variant ='top' src={book_image} alt={title}></Card.Img>
+//                     <Card.Text>{title}</Card.Text>
+//                     <Card.Text>{description}</Card.Text>
+//                     <Card.Text>Writen by: {author}</Card.Text>
+//                       <ul>
+//                         <li>Publisher: {publisher}</li>
+//                         <li>Ranked number {rank}</li>
+//                         <li>ISBN:{primary_isbn10}</li>
+//                       </ul>
+//                       <ul>
+//                         <p>Buy Now:</p>
+//                         {buy_links.map((link) => {
+//                         const{name,url} = link
+                        
+//                         return(
+//                           <div key={name}>
+//                             <a href={url}>{name}</a>
+//                           </div>
+//                         )
+//                         })}
+//                     </ul>
+//                  </div>
+//                 </Card.Body>
+//               </Card>
+         
+         
+//           </div>
+//             )
+//           })}
+         
+//         </div>
+//     </div>
+//   );
+// }
+
+return (
+  <section id="books" className="books-block">
+    <Container className='border d-flex align-items-center justify-content-center height-50rem'>
+      <div className="title-holder">
+        {/* <h2>Best Sellers</h2> */}
+        
+      </div>
+      
+      <Carousel controls={true}>
+        
           {books.map((book) =>{
-            const{author, book_image, buy_links, primary_isbn10,description, publisher, rank, title}=book
-            return(
-              <article key={rank}>
-                <div>
-                  <img src={book_image} alt={title} />
-                </div>
-                <div>
-                  <h4>{title}</h4>
+            const{author, book_image, buy_links, primary_isbn10,description, publisher, rank, title}= book
+            return (
+              
+           
+              <Carousel.Item className= "book_format" key={rank}>
+              <blockquote>
+               <img className='image'src={book_image} alt={title} />
+                  <h3>{title}</h3>
                   <p>{description}</p>
                   <p>Written by: {author}</p>
-                </div>
-                <div>
-                  <ul>
-                    <li>Publisher: {publisher}</li>
-                    <li>Ranked number {rank}</li>
-                    <li>ISBN:{primary_isbn10}</li>
-                  </ul>
-                </div>
-                <ul>
-                  <p>Buy Now:</p>
-                  {buy_links.map((link) => {
-                    const{name,url} = link
-                  
-                  return(
-                    <div key={name}>
-                      <a href={url}>{name}</a>
-                    </div>
-                  )
-                })}
-                </ul>
-              </article>
-            )
-          })}
-        </section>
-      </div>
-    </div>
-  );
+              </blockquote>
+              </Carousel.Item>
+              
+            
+
+              
+            );
+          })
+        }
+      </Carousel>
+     
+    </Container>
+  </section>
+);
 }
 export default Home;
