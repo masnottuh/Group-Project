a87d267373451b5573349f706ab2e5542522b780
lizcoats <lcclujana@gmail.com>
2023-03-17 10:48:42 -0400

    Homepage design

--- frontend/src/NavPages/Home.js
+++ frontend/src/NavPages/Home.js
@@ -18,88 +18,77 @@ const Home = () =>{
   getBooks()
 }, [])
     
-// return (
-//     <div className="centered">
-//      <h1>Books</h1>
-//       <div>
-     
-//          {books.map((book) =>{
-//             const{author, book_image, buy_links, primary_isbn10,description, publisher, rank, title}= book
-            
-//             return(
-//               <div>
-//               <Card key ={rank} bg='secondary'style={{width: '18rem'}}>
-//               <Card.Body>
-//                 <div>
-//                     <Card.Img className ="image_book" variant ='top' src={book_image} alt={title}></Card.Img>
-//                     <Card.Text>{title}</Card.Text>
-//                     <Card.Text>{description}</Card.Text>
-//                     <Card.Text>Writen by: {author}</Card.Text>
-//                       <ul>
-//                         <li>Publisher: {publisher}</li>
-//                         <li>Ranked number {rank}</li>
-//                         <li>ISBN:{primary_isbn10}</li>
-//                       </ul>
-//                       <ul>
-//                         <p>Buy Now:</p>
-//                         {buy_links.map((link) => {
-//                         const{name,url} = link
-                        
-//                         return(
-//                           <div key={name}>
-//                             <a href={url}>{name}</a>
-//                           </div>
-//                         )
-//                         })}
-//                     </ul>
-//                  </div>
-//                 </Card.Body>
-//               </Card>
-         
-         
-//           </div>
-//             )
-//           })}
-         
-//         </div>
-//     </div>
-//   );
-// }
-
 return (
-  <section id="books" className="books-block">
-    <Container className='border d-flex align-items-center justify-content-center height-50rem'>
-      <div className="title-holder">
-        {/* <h2>Best Sellers</h2> */}
-        
-      </div>
-      
-      <Carousel controls={true}>
-        
-          {books.map((book) =>{
-            const{author, book_image, buy_links, primary_isbn10,description, publisher, rank, title}= book
-            return (
-              
-           
-              <Carousel.Item className= "book_format" key={rank}>
-              <blockquote>
-               <img className='image'src={book_image} alt={title} />
-                  <h3>{title}</h3>
-                  <p>{description}</p>
-                  <p>Written by: {author}</p>
-              </blockquote>
-              </Carousel.Item>
-              
-            
-
-              
-            );
-          })
-        }
-      </Carousel>
+    <div className="centered">
+     <h1>Top 15 Best Sellers Fiction Books</h1>
+    
+      <div className='books-card-info'>
      
-    </Container>
-  </section>
-);
+         {books.map((book) =>{
+            const{author, book_image, buy_links, primary_isbn10,description, publisher, rank, title}= book
+            
+            return(
+              <div >
+              <Card key ={rank} className='m-2 rounded shadow-lg' style={{width:'15rem'}}>
+              <Card.Body>
+                <div>
+                    <Card.Img fluid className ="image_book" variant ='top' src={book_image} alt={title}></Card.Img>
+                    <Card.Text>{description}</Card.Text>
+                    <Card.Text>Writen by: {author}</Card.Text>
+                    <p>Rank: {rank}</p>
+                      {/* <ul>
+                        <li>Publisher: {publisher}</li>
+                        <li>Ranked number {rank}</li>
+                        <li>ISBN:{primary_isbn10}</li>
+                      </ul> */}
+                      <ul>
+                        <p>Buy Now:</p>
+                        {buy_links.map((link) => {
+                        const{name,url} = link
+                        
+                        return(
+                          <div key={name}>
+                            <a href={url}>{name}</a>
+                          </div>
+                        )
+                        })}
+                    </ul>
+                 </div>
+                </Card.Body>
+              </Card>
+         
+         
+          </div>
+            )
+          })}
+         
+        </div>
+    </div>
+  );
 }
 export default Home;
+// return (
+//   <section id="books" className="books-block">
+//     <Container className='border d-flex          align-items-center justify-content-center height-30rem'>
+//       <Carousel controls={true}>
+//           {books.map((book) =>{
+//             const{author, book_image, buy_links, primary_isbn10,description, publisher, rank, title}= book
+            
+//             return (
+//              <Carousel.Item className= "book_format" key={rank}>
+//                <img className='image'src={book_image} alt={title} />
+//                   <h5>{title}</h5>
+//                   <blockquote>
+//                   <p>{description}</p>
+//                   </blockquote>
+//                   <p>Written by: {author}</p>
+//              </Carousel.Item>
+//             );
+//           })
+//         }
+//       </Carousel>
+//     </Container> 
+//   </section>
+// );
+// }
+// export default Home;
