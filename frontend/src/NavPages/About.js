8f0840002cfc31b93070416eabe8bf0cc24048f0
Sam Z <samzilli01@gmail.com>
2023-03-18 13:32:32 -0400

    added a short mission statement

--- frontend/src/NavPages/About.js
+++ frontend/src/NavPages/About.js
@@ -1,11 +1,17 @@
-
 import React from 'react';
+
 const About = () =>{
   return (
     <div className="centered">
       <h3>About Book Club</h3>
       <div>
         Book Club
+        <img src={require("../images/bc_logo.png")} alt="Book Club Logo" width='100%'/>
+        <div>
+        Founded by 4 graduates of Code Platoon's Full-Stack Engineering Program, {'\n'}
+        BookClub aims to bring all of the charm and community of a local book club
+        to readers around the world.
+        </div>
       </div>
     </div>
   );
