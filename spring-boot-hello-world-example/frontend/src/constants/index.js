3d593e0a73c3d808d58667baf48caee53856c26d
Robert Hutton <sammyhutton543@gmail.com>
2023-03-30 15:41:11 -0700

    deletes

--- spring-boot-hello-world-example/frontend/src/constants/index.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = "http://127.0.0.1:8000/api/";
-
-export const SIGNUP_URL = "http://127.0.0.1:8000/authentication/signup";
-
-export const TOKEN_URL = "http://127.0.0.1:8000/authentication/get-token";
