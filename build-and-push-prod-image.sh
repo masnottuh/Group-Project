00fcad5826734837a76bd8250653f6a2888977e5
Robert Hutton <sammyhutton543@gmail.com>
2023-03-30 15:54:10 -0700

    deployment structure

--- build-and-push-prod-image.sh
+++ build-and-push-prod-image.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+
+##############################
+# This builds and pushes our application image
+#
+# The nginx/React static files get built with an environment variable
+# that sets the url of the DRF backend REACT_APP_BASE_URL.  Once you
+# know the IP address of your EC2 instance, you would pass that in
+# instead of localhost
+##############################
+
 BASE_URL=$1
 NEW_VERSION=$2
 
-docker buildx build --platform linux/amd64 --build-arg REACT_APP_BASE_URL=$BASE_URL -t masnottuh/webserver-prod:$NEW_VERSION -f webserver/Dockerfile . --no-cache
-docker push masnottuh/webserver-prod:$NEW_VERSION
+docker buildx build --platform linux/amd64 --build-arg REACT_APP_BASE_URL=$BASE_URL -t masnottuh/complete-group:$NEW_VERSION . --no-cache
 
-docker buildx build --platform linux/amd64  -t masnottuh/backend-prod:$NEW_VERSION -f backend/Dockerfile ./backend --no-cache
-docker push masnottuh/backend-prod:$NEW_VERSION
+docker push masnottuh/complete-group:$NEW_VERSION
