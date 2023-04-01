266531608cfd3c20daeb3cb2954f7333ca8aa092
Sam Z <samzilli01@gmail.com>
2023-04-01 13:16:41 -0400

    added comment icon

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,16 +1,16 @@
 
-// local host
-export const API_URL = "http://localhost/api/";
+// // local host
+// export const API_URL = "http://localhost/api/";
 
-export const SIGNUP_URL = "http://localhost/api/authentication/signup";
+// export const SIGNUP_URL = "http://localhost/api/authentication/signup";
 
-export const TOKEN_URL = "http://localhost/api/authentication/get-token";
+// export const TOKEN_URL = "http://localhost/api/authentication/get-token";
 
-export const BOOK_URL = 'http://localhost/api/api/book/';
+// export const BOOK_URL = 'http://localhost/api/api/book/';
 
-export const REVIEW_URL = 'http://localhost/api/api/reviews/';
+// export const REVIEW_URL = 'http://localhost/api/api/reviews/';
 
-export const COMMENT_URL = 'http://localhost/api/api/comments/';
+// export const COMMENT_URL = 'http://localhost/api/api/comments/';
 
 
 // AWS instance:
@@ -26,3 +26,19 @@ export const COMMENT_URL = 'http://localhost/api/api/comments/';
 // export const REVIEW_URL = 'http://34.221.117.152//api/api/reviews/';
 
 // export const COMMENT_URL = 'http://34.221.117.152//api/api/comments/';
+
+
+
+
+
+export const API_URL = "http://localhost:8000/api/";
+
+export const SIGNUP_URL = "http://localhost:8000/authentication/signup";
+
+export const TOKEN_URL = "http://localhost:8000/authentication/get-token";
+
+export const BOOK_URL = 'http://localhost:8000/api/book/';
+
+export const REVIEW_URL = 'http://localhost:8000/api/reviews/';
+
+export const COMMENT_URL = 'http://localhost:8000/api/comments/';
